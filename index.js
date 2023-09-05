d39a00149311386ca0d598610f5450af48e9d391
Gabriel Rios16 <gabrierioos1609@gmail.com>
2023-09-05 19:47:45 -0300

    Primeira Versão

---
+++ index.js
@@ -0,0 +1,23 @@
+var Leonardo=window.document.getElementById("img-leo")
+var Samanta=window.document.getElementById("img-samanta")
+var Bruna=window.document.getElementById("img-bruna")
+var SetaDireita=window.document.getElementById("seta-direita")
+var SetaEsquerda=window.document.getElementById("seta-esquerda")
+function RolarParaDireita(){
+    Leonardo.style="display:none"
+    Bruna.style="display:flex"
+    SetaEsquerda.style="display:flex"
+    SetaDireita.style="display:none"
+}
+function RolarParaEsquerda(){
+    Leonardo.style="display:flex"
+    Bruna.style="display:none"
+    SetaEsquerda.style="display:none"
+    SetaDireita.style="display:flex"
+}
+
+var ImagemEmenta=window.document.getElementById("imagem-ementa")
+
+function AcessarEmenta(){
+    
+}
